53cd4f6ca6acc83da2d6497b457a5084d60eb592
Gekata-2 <60921872+Gekata-2@users.noreply.github.com>
2021-03-16 11:07:39 +0300

    Add files via upload

---
+++ Text/Text/Text.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "MainConstantsEnum.h"
+#include "AtomicTextLink.h"
+int main()
+{
+	char string_1[20];
+	TAtomicTextLink test_text();
+    std::cout << "Hello World!\n";
+}
+
+
+
+
