22132f2115eb1f07d2ec5deb5eb96e860bcbebd5
Gekata-2 <60921872+Gekata-2@users.noreply.github.com>
2021-03-16 11:06:24 +0300

    Add files via upload

---
+++ Text/Text/AtomicTextLink.cpp
@@ -0,0 +1,33 @@
+#include <conio.h>
+#include "AtomicTextLink.h"
+#include "Text.h"
+
+//Инициализацтя памяти
+void TAtomicTextLink::InitializationMemorySystem(int size)
+{
+	char Line[100];//строка - весь текст(состоит как бы из звеньев-строк)
+	pointer_header.p_first_link = (PTAtomicTextLink) new char[sizeof(TAtomicTextLink) * size];/*создание строки размером=размер каждой строки*количество строк в тексте
+	указатель pointer_header.p_first_link указывает на начало этой строки,а именно на структуру размером со звено TAtomicTextLink
+	*/
+	pointer_header.p_first_free_link = pointer_header.p_first_link;//указывает туда же куда и pointer_header.p_first_link
+	pointer_header.p_last_link = pointer_header.p_first_link + (size - 1);/*указыват на последний участок текста,
+	т.е. мы от указателя на превую строку перемещаемся на (size - 1) строк по тексту вперёд*/
+	PTAtomicTextLink p_link = pointer_header.p_first_link;//создаём указатель типа "указатель на звено".Он указывает на начало текста,т.е на первую строку
+	//размерка памяти
+	for (int i = 0; i < size- 1; i++,p_link++)
+	{
+		p_link->p_next = p_link + 1;//p_link->p_next=(*p_link).p_nest
+		/*
+		Т.к p_link указывает на объект типа TAtomicTextLink, 
+		то при p_link->p_next мы обращаемся к полю-указателю p_next объекта TAtomicTextLink и призваиваем ему адрес следующего объекта типа TAtomicTextLink
+				___________str1__________________			___________str2__________________       ___________str3__________________
+		p_link->{p_down|str|pointer_header|p_next}		--->{p_down|str|pointer_header|p_next}		--->{p_down|str|pointer_header|p_next} 
+											|->---------^								|->---------^								|->---------^
+			На каждой итерации цикла 	p_link перемещается на следующую строку и как следствие у каждой строки есть указатель на следующую строку					
+		
+		*/
+	}
+	p_link->p_next = NULL;//после того как мы указали всем строкам, кроме последней, где находится следующая ,указываем  последней что следующей за ней строки не существует
+
+	//Таким образом все первичные указатели указывают на нужные конструкции и звенья
+}
---
+++ Text/Text/CompletionCodesDefine.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define TEXT_OK 0 //Ошибок нет	
+		//Коды ситуаций
+#define TEXT_NO_DOWN_LEVEL_EXIST 101		//Нет подуровоня для текущей позиции
+#define TEXT_NO_NEXT_SAME_LEVEL_EXIST 102	//Нет следующего раздела текущего уровня
+#define TEXT_NO_PREV_LEVEL_EXIST 103		//Нет уровней выще(Текущая позиция в начале текста)
+		//Коды ошибок
+#define TEXT_ERROR_IN_TEXT -102				//Ошибка в тексте
+#define TEXT_MEMORY_OWERFLOW  -101			//Кончилась память
